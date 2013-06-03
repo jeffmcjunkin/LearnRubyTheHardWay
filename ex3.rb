eca1690eb5e833d6d0f722f00176ff5b8e630237
Jeff McJunkin <jeff.mcjunkin@gmail.com>
2013-06-02 17:01:28 -0700

    Fixed a silly typo.

--- ex3.rb
+++ ex3.rb
@@ -14,7 +14,7 @@ puts 3 + 2 < 5 - 7
 puts "What is 3 + 2?", 3 + 2
 puts "What is 5 - 7?", 5 - 7
 
-puts "Oh, that's why why it's false."
+puts "Oh, that's why it's false."
 
 puts "How about some more."
 
